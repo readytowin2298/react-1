fd7935a5e26bc40e21ad3226c25de508be44ad93
David Ames <readytowin2298@gmail.com>
2021-05-18 12:46:36 -0500

    addCommas, done and dusted

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,16 @@
-function addCommas() {}
+function addCommas(num) {
+    const numString = `${num}`;
+    const len = numString.length;
+    let finished = ""
+    for(let i=0; i<len; i++){
+        finished += numString[i]
+        if( i !== (len - 1) &&  (len - 1 - i)%3 === 0){
+            finished += ","
+        }
+    }
+    return finished;
+}
 
 module.exports = addCommas;
+
+// 01234567
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,13 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+  test("Puts commas in whole numbers", () => {
+    expect(addCommas(1234)).toBe("1,234");
+    expect(addCommas(1000000)).toBe("1,000,000");
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+    expect(addCommas(6)).toBe("6");
+    expect(addCommas(-10)).toBe("-10");
+    expect(addCommas(-5678)).toBe("-5,678");
+    // expect(addCommas())
+  })
 });
